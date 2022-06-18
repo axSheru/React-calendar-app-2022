6ffb143b2eb51a3c14f9d4ca8812616e05783071
Alex Paredes <alexparedesr95@gmail.com>
2022-06-17 19:09:27 -0500

    REFACTOR: Simplified calendarPage component using helpers.

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,20 +1,8 @@
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import { Calendar } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import { addHours, format, parse, startOfWeek, getDay } from 'date-fns/esm';
-import enUS from 'date-fns/locale/en-US';
+import { addHours } from 'date-fns/esm';
 import { Navbar } from "../";
-
-const locales = {
-    'en-US': enUS,
-};
-
-const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-  locales,
-});
+import { getMessagesES, localizer } from '../../helpers';
 
 const events =[
     {
@@ -32,14 +20,33 @@ const events =[
 
 
 export const CalendarPage = () => {
+
+    const eventStyleGetter = ( event, start, end, isSelected ) => {
+
+        const style = {
+            backgroundColor: '#347CF7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            color: 'white'
+        };
+
+        return {
+            style
+        };
+
+    };
+
     return (
         <>
             <Navbar />
             <Calendar
-                localizer={localizer}
-                events={ events }
-                startAccessor="start"
+                culture='es'
                 endAccessor="end"
+                eventPropGetter={ eventStyleGetter }
+                events={ events }
+                localizer={ localizer }
+                messages={ getMessagesES() }
+                startAccessor="start"
                 style={{ height: 'calc( 100vh - 80px )' }}
             />
         </>
