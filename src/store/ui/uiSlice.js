ca6596f03707cbeb486e7671fb3c6af47a031ef3
Alex Paredes <alexparedesr95@gmail.com>
2022-06-26 22:57:18 -0500

    FIX: Changed uiSlice name.

--- src/store/ui/uiSlice.js
+++ src/store/ui/uiSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export const uiSlice = createSlice({
-    name: 'name',
+    name: 'ui',
     initialState: {
         isDateModalOpen: false
     },
