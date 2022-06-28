7c78904d44facc8e74bc713349386b339db4b495
Alex Paredes <alexparedesr95@gmail.com>
2022-06-27 23:04:11 -0500

    FEAT: Created method to handle onSetActiveEvent reducer.

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,15 +1,23 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { onSetActiveEvent } from "../store";
 
 export const useCalendarStore = () => {
 
+    const dispatch = useDispatch();
+
     const { events, activeEvent } = useSelector( state => state.calendar );
+
+    const setActiveEvent = ( calendarEvent ) => {
+        dispatch( onSetActiveEvent( calendarEvent ) );
+    };
 
     return {
         //* Propiedades.
         events,
-        activeEvent
+        activeEvent,
 
         //*Métodos.
+        setActiveEvent,
     };
     
 };
