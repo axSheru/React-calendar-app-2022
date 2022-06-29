040c11c6ea4ff7a4dde9993829098533768e292a
Alex Paredes <alexparedesr95@gmail.com>
2022-06-28 21:38:44 -0500

    FEAT: Created component FABAddNew to handle the new event modal.

---
+++ src/calendar/components/FABAddNew.jsx
@@ -0,0 +1,33 @@
+import { addHours } from "date-fns";
+import { useCalendarStore, useUIStore } from "../../hooks";
+
+
+export const FABAddNew = () => {
+
+    const { openDateModal } = useUIStore();
+    const { setActiveEvent } = useCalendarStore();
+
+    const handleClickNew = () => {
+        setActiveEvent({
+            title: '',
+            notes: '',
+            start: new Date(),
+            end: addHours( new Date(), 2 ),
+            bgColor: '#DEA1F9',
+            user: {
+                _id: 123,
+                name: 'Alex'
+            }
+        });
+        openDateModal();
+    };
+
+    return (
+        <button
+            className="btn btn-primary fab"
+            onClick={ handleClickNew }
+        >
+            <i className="fas fa-plus"></i>
+        </button>
+    );
+};
