4867b5d5b06d2c65c35aef765b079c65d97aa07c
Alex Paredes <alexparedesr95@gmail.com>
2022-06-30 20:56:58 -0500

    FEAT: Created new reducer to handle updateEvent event.

--- src/store/calendar/calendarSlice.js
+++ src/store/calendar/calendarSlice.js
@@ -29,10 +29,17 @@ export const calendarSlice = createSlice({
         onAddNewEvent: ( state, { payload } ) => {
             state.events.push( payload );
             state.activeEvent = null;
+        },
+        onUpdateEvent: ( state, { payload } ) => {
+            state.events = state.events.map( event => {
+                return ( event._id === payload._id )
+                    ? payload
+                    : event;
+            });
         }
     }
 });
 
 
 // Action creators are generated for each case reducer function
-export const { onSetActiveEvent, onAddNewEvent } = calendarSlice.actions;
+export const { onSetActiveEvent, onAddNewEvent, onUpdateEvent } = calendarSlice.actions;
