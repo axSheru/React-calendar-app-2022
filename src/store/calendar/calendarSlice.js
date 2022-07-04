a3a537024a6bd9e587046a21899c21eccbe64197
Alex Paredes <alexparedesr95@gmail.com>
2022-07-03 22:04:39 -0500

    FEAT: Created reducer to handle deleteEvent.

--- src/store/calendar/calendarSlice.js
+++ src/store/calendar/calendarSlice.js
@@ -36,10 +36,15 @@ export const calendarSlice = createSlice({
                     ? payload
                     : event;
             });
+        }, onDeleteEvent: ( state ) => {
+            if ( state.activeEvent ) {
+                state.events = state.events.filter( event => event._id !== state.activeEvent._id );
+                state.activeEvent = null;
+            }
         }
     }
 });
 
 
 // Action creators are generated for each case reducer function
-export const { onSetActiveEvent, onAddNewEvent, onUpdateEvent } = calendarSlice.actions;
+export const { onSetActiveEvent, onAddNewEvent, onUpdateEvent, onDeleteEvent } = calendarSlice.actions;
