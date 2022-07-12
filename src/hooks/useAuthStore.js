30b6b787a94737b6337184ed12c4a4d57dbedcbc
Alex Paredes <alexparedesr95@gmail.com>
2022-07-11 23:03:28 -0500

    FEAT: Created function to check token.

--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -47,6 +47,23 @@ export const useAuthStore = () => {
 
     };
 
+    const checkAuthToken = async () => {
+
+        const token = localStorage.getItem( 'token' );
+        if ( !token ) return dispatch( onLogout() );
+
+        try {
+            const { data } = await calendarApi.get( '/auth/renew' ); 
+            localStorage.setItem( 'token', data.token );
+            localStorage.setItem( 'token-init-date', new Date().getTime() );
+            dispatch( onLogin({ name: data.name, uid: data.uid }) );
+        } catch (error) {
+            localStorage.clear();
+            dispatch( onLogout() );
+        }
+
+    };
+
     
     return {
         //* Properties.
@@ -55,6 +72,7 @@ export const useAuthStore = () => {
         errorMessage,
         
         //* Methods.
+        checkAuthToken,
         startLogin,
         startRegister,
     };
