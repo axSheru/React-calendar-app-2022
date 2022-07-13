598d4275a4d4a3e8aed2694408e34c6732158dab
Alex Paredes <alexparedesr95@gmail.com>
2022-07-12 22:18:39 -0500

    CHORE: Imported react-loader-spinner styles.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { CalendarApp } from './CalendarApp';
-import './styles.css'
+import './styles.css';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <CalendarApp />
