6fbb38c2fbb187c427e2bc40ea81823b747ef452
Alex Paredes <alexparedesr95@gmail.com>
2022-08-14 21:14:32 -0500

    TEST: Added test for authenticated and not-authenticated cases on AppRouter component.

--- tests/router/AppRouter.test.jsx
+++ tests/router/AppRouter.test.jsx
@@ -1,8 +1,15 @@
 import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { CalendarPage } from "../../src/calendar";
 import { useAuthStore } from "../../src/hooks/useAuthStore";
 import { AppRouter } from "../../src/router/AppRouter";
 
 jest.mock( '../../src/hooks/useAuthStore' );
+
+// Hacemos un mock del componente a renderizar para no tener que mockear todo lo que originalmente usa dicho componente.
+jest.mock( '../../src/calendar', () => ({
+    CalendarPage: () => <h1>CalendarPage</h1>
+}));
 
 describe('Pruebas en el componente <AppRouter />', () => {
 
@@ -20,6 +27,41 @@ describe('Pruebas en el componente <AppRouter />', () => {
         render( <AppRouter/> );
         expect( screen.getByText('Cargando...') ).toBeTruthy();
         expect( mockCheckAuthToken ).toHaveBeenCalled();
+
+    });
+
+    test('Debe de mostrar el login en caso de no estar autenticado.', () => {
+
+        useAuthStore.mockReturnValue({
+            status: 'not-authenticated',
+            checkAuthToken: mockCheckAuthToken
+        });
+
+        const { container } = render(
+            <MemoryRouter initialEntries={[ '/auth2/algo' ]}>
+                <AppRouter/>
+            </MemoryRouter>
+        );
+
+        expect( screen.getByText( 'Ingreso' ) ).toBeTruthy();
+        expect( container ).toMatchSnapshot();
+
+    });
+
+    test('Debe de mostrar el calendario en caso de estar autenticado.', () => {
+
+        useAuthStore.mockReturnValue({
+            status: 'authenticated',
+            checkAuthToken: mockCheckAuthToken
+        });
+
+        render(
+            <MemoryRouter>
+                <AppRouter/>
+            </MemoryRouter>
+        );
+
+        expect( screen.getByText( 'CalendarPage' ) ).toBeTruthy();
 
     });
 
